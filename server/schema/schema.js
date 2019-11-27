bab6b364d52f4a89457b226f37afb41b7278c7a5
richanynguon <alina.nguon@gmail.com>
2019-11-27 04:50:08 -0500

    add lodash

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,12 @@
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const _ = require("lodash");
+
+//dummy data
+var books = [
+	{ name: "aye1", genre: "aye1", id: "1" },
+	{ name: "aye2", genre: "aye2", id: "2" },
+	{ name: "aye3", genre: "aye2", id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
 	name: "Book",
@@ -17,6 +25,7 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLString } },
 			resolve(parent, args) {
 				// code to get data from the db/other source
+				return _.find(books, { id: args.id });
 			}
 		}
 	}
